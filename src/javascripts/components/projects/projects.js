14479fd0ec09d4fed50e11db5ef0ce5a41bfd0ee
Steven Zelenak <stevenmzelenak@gmail.com>
2020-04-02 22:17:03 -0500

    updated my website projects page

--- src/javascripts/components/projects/projects.js
+++ src/javascripts/components/projects/projects.js
@@ -6,14 +6,21 @@ const createProjectCards = () => {
   let domString = '';
 
   projects.forEach((project) => {
-    domString += '<div class = "cards cards-bg">';
-    domString += `<h2>${project.title}</h2> `;
-    domString += '<div class = "cardsContent">';
-    domString += `<div class = "imgDiv"><img src=${project.screenshot} alt=""></div>`;
-    domString += `<p>${project.description}</p> `;
-    domString += `<p>${project.technologiesUsed}</p> `;
-    domString += `<p>${project.url}</p> `;
-    domString += `<p>${project.githubUrl}</p> `;
+    domString += '<div class="card mb-5 projectCards project-text" style="max-width: 100%;">';
+    domString += '<div class="row no-gutters">';
+    domString += '<div class="col-md-6">';
+    domString += `<img src=${project.screenshot} class="card-img" alt="...">`;
+    domString += '</div>';
+    domString += '<div class="col-md-6">';
+    domString += '<div class="card-body">';
+    domString += `<h5 class="card-title">${project.title}</h5>`;
+    domString += `<p class="card-text">${project.description}</p>`;
+    domString += '<div class="my-2">';
+    domString += `<a href=${project.githubUrl}><i class="github-icon mx-3 fa-3x fab fa-github"></i></a>`;
+    domString += `<a href=${project.githubUrl}><i class="firebase-icon mx-3 fa-3x fas fa-fire"></i></a>`;
+    domString += '</div>';
+    domString += `<p class="card-text"><small class="tech-text">${project.technologiesUsed}</small></p>`;
+    domString += '</div>';
     domString += '</div>';
     domString += '</div>';
     domString += '</div>';
--- src/javascripts/helpers/data/projectsData.js
+++ src/javascripts/helpers/data/projectsData.js
@@ -1,12 +1,21 @@
 const projects = [{
   title: 'Tamagotchi',
-  screenshot: '../../../Screenshots/tamoScreen.png',
-  description: `In this project we used what we learned of ES6 modules and SASS to create a Tamagotchi. 
+  screenshot: '../../../../Screenshots/tamoScreen.png',
+  description: `In this project I used what I learned of ES6 modules and SASS to create a Tamagotchi. 
   The Tamagotchi has four values eat, fight, play, sleep. Which go up or down depending on the button you click.`,
-  technologiesUsed: 'HTML, SCSS, JavaScript, Modules, Version Control with Github',
+  technologiesUsed: 'HTML, SCSS, JavaScript, ES6 Modules, Version Control with Github',
   available: true,
   url: 'https://github.com/StevenZelenak/tamagotchi-project',
   githubUrl: 'https://github.com/StevenZelenak/tamagotchi-project',
+},
+{
+  title: 'Hat-Exercise',
+  screenshot: '../../../../Screenshots/hatScreen.png',
+  description: 'This project is an exploration into Bootstrap 4. I used a Bootstrap jumbotron to display a form that can create bootstrap cards with different house names from Harry Potter. The color of each card is based on which house you get and you can also expel a card from the collection.',
+  technologiesUsed: 'HTML, Bootstrap 4, JavaScript, Version Control with Github',
+  available: true,
+  url: 'https://github.com/StevenZelenak/hat-exercise',
+  githubUrl: 'https://github.com/StevenZelenak/hat-exercise',
 },
 {
   title: 'Cool Project',
