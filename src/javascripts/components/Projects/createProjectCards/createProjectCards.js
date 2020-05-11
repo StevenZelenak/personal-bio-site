14111bb693a209f2183dc96aee7a6f1fbdc740a6
Steven Zelenak <stevenmzelenak@gmail.com>
2020-05-10 20:43:20 -0500

    wip

--- src/javascripts/components/Projects/createProjectCards/createProjectCards.js
+++ src/javascripts/components/Projects/createProjectCards/createProjectCards.js
@@ -11,7 +11,7 @@ const createProjectCards = (project) => {
   domString += `<p class="card-text">${project.description}</p>`;
   domString += '<div class="my-2">';
   domString += `<a href=${project.githubUrl}><i class="github-icon mx-3 fa-3x fab fa-github"></i></a>`;
-  domString += `<a href=${project.githubUrl}><i class="firebase-icon mx-3 fa-3x fas fa-fire"></i></a>`;
+  domString += `<a href=${project.url}><i class="firebase-icon mx-3 fa-3x fas fa-fire"></i></a>`;
   domString += '</div>';
   domString += `<p class="card-text"><small class="tech-text">${project.technologiesUsed}</small></p>`;
   domString += '</div>';
