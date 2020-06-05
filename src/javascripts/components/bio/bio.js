789140375ce36547c2337462dfb180993c55bb6a
Steven Zelenak <stevenmzelenak@gmail.com>
2020-06-04 20:45:30 -0500

    wip

--- src/javascripts/components/bio/bio.js
+++ src/javascripts/components/bio/bio.js
@@ -3,13 +3,28 @@ import './bio.scss';
 
 const buildBioPage = () => {
   let domString = '';
-  domString += '<div class="container">';
+  domString += '<div class="bio-container">';
   domString += '<div>';
-  domString += '<img src="https://i.imgur.com/KMZHYxC.jpg" class="center mb-5 mt-4" alt="...">';
+  domString += '<img src="https://i.imgur.com/KMZHYxC.jpg" class="center mb-5 mt-4 bio-img" alt="...">';
   domString += '</div>';
   domString += '<div class="px-5 py-5 bio">';
   // eslint-disable-next-line quotes
-  domString += `<p> I have been playing video-games for as long as I can remember. When I was younger I would constantly draw up designs for what I would put in a game if I had the ability to make one. As life went on I tried to learn to code on and off thinking just a little more and I will finally be able to make a game. Except the dreaded higher concepts of coding came in and said "You shall not pass!". I was about to give up hope on ever learning to code, that was when I heard about Nashville Software School. I didn't believe I had what it took to become a programmer but thanks to the teachers at NSS I can't believe I didn't go to school sooner. Everyday the concepts get more challenging but I can't think of anything else I would rather be doing right now.`;
+  domString += `<p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a 
+  galley of type and scrambled it to make a type specimen book. It has
+  survived not only five centuries, but also the leap into electronic typesetting, remaining
+   essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets
+    containing Lorem Ipsum passages, and more recently with desktop publishing
+  software like Aldus PageMaker including
+   versions of Lorem Ipsum.Lorem Ipsum is simply dummy te
+   xt of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+  of type and scrambled it to make a type specimen book. It 
+  has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing
+  Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of 
+  Lorem Ipsum.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy
+  text ever since the 1500s, when an unknown printer took a galley of type and 
+  scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised
+  in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>`;
   domString += '</div>';
   domString += '</div>';
 
