187ecb7b78eb584c1fa0c039bc56c4f39fadda31
Steven Zelenak <stevenmzelenak@gmail.com>
2021-04-24 15:19:21 -0500

    wip

--- src/javascripts/components/technologies/tech.js
+++ src/javascripts/components/technologies/tech.js
@@ -5,36 +5,7 @@ const buildTechPage = () => {
   let domString = '';
 
   domString += '<div class="skillDiv">';
-  domString += '<p>HTML</p>';
-  domString += '<div class="skillContainer">';
-  domString += '<div class="skills html">80%</div>';
-  domString += '</div>';
-
-  domString += '<p>CSS</p>';
-  domString += '<div class="skillContainer">';
-  domString += '<div class="skills css">45%</div>';
-  domString += '</div>';
-
-  domString += '<p>JavaScript</p>';
-  domString += '<div class="skillContainer">';
-  domString += '<div class="skills js">65%</div>';
-  domString += '</div>';
-
-  domString += '<p>Firebase</p>';
-  domString += '<div class="skillContainer">';
-  domString += '<div class="skills firebase">15%</div>';
-  domString += '</div>';
-
-  domString += '<p>Github</p>';
-  domString += '<div class="skillContainer">';
-  domString += '<div class="skills github">40%</div>';
-  domString += '</div>';
-
-  domString += '<p>Bootstrap</p>';
-  domString += '<div class="skillContainer">';
-  domString += '<div class="skills bootstrap">50%</div>';
-  domString += '</div>';
-
+  domString += '<p>HTML5 || CSS3 || Javascript || Bootstrap 4 || Firebase || Github || React || C# || .Net</p>';
   domString += '</div>';
 
   utils.printToDom('sz-tech', domString);
