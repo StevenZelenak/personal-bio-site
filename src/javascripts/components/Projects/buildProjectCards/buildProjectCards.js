782c44f588009fe6cac13fd01c10a5cdcb0a6e94
Steven Zelenak <stevenmzelenak@gmail.com>
2021-04-27 22:23:51 -0500

    non firebase code

--- src/javascripts/components/Projects/buildProjectCards/buildProjectCards.js
+++ src/javascripts/components/Projects/buildProjectCards/buildProjectCards.js
@@ -3,17 +3,15 @@ import utils from '../../../helpers/utils';
 import createProjectCards from '../createProjectCards/createProjectCards';
 
 const buildProjects = () => {
-  projectData.getProjects()
-    .then((projects) => {
-      let domString = '';
-      domString += '<div id="projectsPage" class="container py-2 px-2 rounded">';
-      projects.forEach((project) => {
-        domString += createProjectCards.createProjectCards(project);
-      });
-      domString += '</div>';
-      utils.printToDom('sz-projects', domString);
-    })
-    .catch((err) => console.error('get cows broke', err));
+  const projects = projectData.getProjects;
+  console.log(projects);
+  let domString = '';
+  domString += '<div id="projectsPage" class="container py-2 px-2 rounded">';
+  projects.forEach((project) => {
+    domString += createProjectCards.createProjectCards(project);
+  });
+  domString += '</div>';
+  utils.printToDom('sz-projects', domString);
 };
 
 export default {
--- src/javascripts/helpers/data/projectData.js
+++ src/javascripts/helpers/data/projectData.js
@@ -1,22 +1,49 @@
-import axios from 'axios';
-import apiKeys from '../apiKeys.json';
-
-const baseURL = apiKeys.firebaseKeys.databaseURL;
-
-const getProjects = () => new Promise((resolve, reject) => {
-  axios.get(`${baseURL}/projects.json`)
-    .then((response) => {
-      const theProjects = response.data;
-      const projects = [];
-      Object.keys(theProjects).forEach((projectId) => {
-        theProjects[projectId].id = projectId;
-        projects.push(theProjects[projectId]);
-      });
-
-      resolve(projects);
-    })
-    .catch((err) => reject(err));
-});
+const getProjects = [
+  {
+    title: 'Book Keeper',
+    screenshot: 'https://i.imgur.com/yWTt3iP.png',
+    description:
+      'I created this site with the idea of storing a list of all my books digitally. Whether they are digital, physical, or audio. When the site it opened you are taken to your library where you can Create a book, Update that book, and Delete the book. The reason for creating this app was because I wanted a way to check my library away from home so not to double purchase any books. Which is something that has happened to me quite often.',
+    technologiesUsed:
+      'React, SASS, JSX, Firebase, React-Bootstrap, Version Control with Github',
+    available: true,
+    url: 'https://book-keeper-5e44c.web.app/auth',
+    githubUrl: 'https://github.com/StevenZelenak/book-keeper',
+  },
+  {
+    title: 'Hat-Exercise',
+    screenshot: 'https://i.imgur.com/pu8IPNv.png',
+    description:
+      'This project is an exploration into Bootstrap 4. I used a Bootstrap jumbotron to display a form that can create bootstrap cards with different house names from Harry Potter. The color of each card is based on which house you get and you can also expel a card from the collection.',
+    technologiesUsed:
+      'HTML, Bootstrap 4, JavaScript, Version Control with Github',
+    available: true,
+    url: 'https://hat-exercise.firebaseapp.com',
+    githubUrl: 'https://github.com/StevenZelenak/hat-exercise',
+  },
+  {
+    title: 'Pinterest',
+    screenshot: 'https://i.imgur.com/jhkeZPz.png',
+    description:
+      'This project is an exploration into FireBase. I used cards to display information pulled from Firebase, you can edit, create, or delete the cards. The information also shows up differently depending on what user is logged in.',
+    technologiesUsed:
+      'HTML, Bootstrap 4, JavaScript, Version Control with Github, Firebase',
+    available: true,
+    url: 'https://pinterest-c6acf.firebaseapp.com',
+    githubUrl: 'https://github.com/StevenZelenak/pinterest',
+  },
+  {
+    title: 'Mushroom Picker',
+    screenshot: 'https://i.imgur.com/aqxbDqT.png',
+    description:
+      'This project is an exploration into React. You click the pick a shroom button to make a random mushroom and different effects happen depending on which mushroom you get. The goal is to collect all the mushrooms.',
+    technologiesUsed:
+      'HTML, Bootstrap 4, JavaScript, Version Control with Github, React, Sass',
+    available: true,
+    url: 'https://mushroom-picker-f8c05.web.app',
+    githubUrl: 'https://github.com/StevenZelenak/mushroom-picker',
+  },
+];
 
 export default {
   getProjects,
